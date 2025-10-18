2ef3a8e27c8c9ff1a7ae599b2e22be68c970847a
Liu GuangYang <lgybtm@foxmail.com>
2025-10-18 16:24:42 +0800

    fix merge

--- lab00/code.py
+++ lab00/code.py
@@ -7,7 +7,7 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
-  if num%3 == 0 and num%5 == 0:
+  if num%15 == 0:
     print(f"{num}: fizzbuzz")
   if num % 3 == 0: # edit this line
     print(f"{num}: fizz")
