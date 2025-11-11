1f5a8b3b4923df977ed24efe7fbec8b3f15fa3fa
Liu GuangYang <lgybtm@foxmail.com>
2025-11-11 21:19:09 +0800

    finish lab03.

--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 1
 
 .text
 # Don't worry about understanding the code in main
@@ -27,7 +27,14 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    addi t0 x0 1  # let result = 1;
+    BEQ a0 x0 finish
+loop:
+    BEQ a0 x0 finish
+    mul t0 t0 a0 # result = result * x;
+    addi a0 a0 -1 # i--;
+    j loop
+finish: mv a0 t0
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
